054b07df7406ba22ec122eb045ae7949dc82e8a9
kekee000 <kekee000@gmail.com>
2015-01-12 20:36:25 +0800

    add fonteditor doc

---
+++ src/main.js
@@ -0,0 +1,25 @@
+/**
+ * @file main.js
+ * @author mengke01
+ * @description
+ * 帮助文档入口
+ */
+
+define(
+    function (require) {
+
+        var entry = {
+
+            /**
+             * 初始化
+             */
+            init: function () {
+
+            }
+        };
+
+        entry.init();
+
+        return entry;
+    }
+);
